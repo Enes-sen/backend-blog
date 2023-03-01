f4ef86a246ff4d72b0498459b3cfa20f45b93f15
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-01 16:39:55 +0300

    Update routes.js

--- router/routes.js
+++ router/routes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const { getposts,getsinglepost, createpost, setpost, deletepost } = require("../controllers/blogcontroller.js");
 
+router.get("/",(req,res)=>{
+    res.setHeader("Access-Control-Allow-Credentials","true");
+    res.send("blog-api");
+
+});
 router.get("/posts", getposts);
 router.get("/posts/:id", getsinglepost);
 router.post("/posts", createpost);
