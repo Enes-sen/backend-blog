23b1db8363bdc82e35761e1b26435a6394cf9e58
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-03 16:06:51 +0300

    Delete commentroutes.js

--- commentroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getPostComments, createComment, deleteComment } = require("../controllers/comentcontroller.js");
-
-router.get("/:id", getPostComments);
-router.post("/:id", createComment);
-router.delete("/:id", deleteComment);
-
-module.exports = router;
