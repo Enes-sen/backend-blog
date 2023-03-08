a08c4e7ddc66843e1cb46a2f0268c0ea946eff3b
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-08 14:00:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ limit:"30mb", extended: true }));
 app.use(bodyParser.json({limit:"30mb", extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://blog-app-r4kr.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use("/",router);
 
 
